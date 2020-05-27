b4d7c0444cd7af4e1394434f6d98c31af496dcc6
jpwf <joaopedroweydt@gmail.com>
2020-05-27 18:53:31 -0300

    Bacl-end atualizado

--- emailgenerator.py
+++ emailgenerator.py
@@ -5,39 +5,39 @@ from time import sleep
 p = 'Deseja gerar um email? '
 nomes = ['maria','joao','mariazinha','jonas','joseph','max','marcio','cleo','marcos','marquinhos','beth','michele','kaka','carlos','jones','marcia','lucca','lucas','locoso','vitor','carolzinha','mikael','lucaco','mikie','maximo','marcinha']
 #definindo listas
-provedor = ['@gmail.com','@hotmail.com','@ig.com']
+email = ['@gmail.com', '@hotmail.com','@ig.com']
 lista1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 lista2 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
 #utilizando function choice da biblioteca random para definir scripts
 def script1():
         a = random.choice(nomes)
-        b = random.choice(provedor)
+        b = random.choice(email)
         c = random.choice(lista1)
         d = random.choice(lista2)
-        z = random.choice(lista1)
-        x = random.choice(lista2)
-        u = random.choice(lista1)
-        i = random.choice(lista2)
-        o = random.choice(lista1)
-        w = random.choice(lista2)
-        v = random.choice(lista1)
-        print('_____LOGIN: {}{}_____'.format(a,b))
-        print('_____SENHA: {}{}{}{}{}{}{}{}{}_____'.format(c,z,x,v,u,d,i,o,w))
-def script2():
         e = random.choice(lista1)
         f = random.choice(lista2)
-        z = random.choice(lista1)
-        x = random.choice(lista2)
-        u = random.choice(lista1)
-        i = random.choice(lista2)
-        o = random.choice(lista1)
-        w = random.choice(lista2)
-        v = random.choice(lista1)
-        print('_____SENHA: {}{}{}{}{}{}{}{}{}_____'.format(e,z,w,i,v,f,o,u,x))
+        g = random.choice(lista1)
+        h = random.choice(lista2)
+        i = random.choice(lista1)
+        j = random.choice(lista2)
+        k = random.choice(lista1)
+        print('_____LOGIN: {}{}_____'.format(a,b))
+        print('_____SENHA: {}{}{}{}{}{}{}{}{}_____'.format(c,e,f,k,g,d,h,i,j))
+def script2():
+        l = random.choice(lista1)
+        m = random.choice(lista2)
+        n = random.choice(lista1)
+        o = random.choice(lista2)
+        p = random.choice(lista1)
+        q = random.choice(lista2)
+        r = random.choice(lista1)
+        s = random.choice(lista2)
+        t = random.choice(lista1)
+        print('_____SENHA: {}{}{}{}{}{}{}{}{}_____'.format(l,n,s,q,t,m,r,p,o))
 def script3():
-        g = random.choice(nomes)
-        h = random.choice(provedor)
-        print('_____LOGIN: {}{}_____'.format(g,h))
+        u = random.choice(nomes)
+        v = random.choice(email)
+        print('_____LOGIN: {}{}_____'.format(u,v))
 def script4():
         p3 = int(input('Digite 3 para gerar mais um email e 4 para gerar uma senha ou login: '))
         if p3 == 3:
@@ -74,11 +74,10 @@ while True:
                                 print('GERANDO LOGIN....')
                                 sleep(2)
                                 script3()
-
                 p2 = str(input('Deseja algo mais? '))
                 if p2 == 's':
                         script4()
                 if p2 == 'n':
                         print('OK, fazendo logoff.....')
                         sleep(1)
-                        break
+                        break 
