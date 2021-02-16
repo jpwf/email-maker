e36ad5739a9d7d1bb0357c902a32210a61768d8d
jpwf <joaopedroweydt@gmail.com>
2021-02-16 16:33:16 -0300

    New version, incomplete, working

--- generator.py
+++ generator.py
@@ -4,7 +4,7 @@ import random
 from time import sleep
 
 question = 'Siga as instruções'
-print("Respostas sempre com s para sim e n para não")
+print("Respostas sempre com s para sim e n para não, sempre entre aspas\nCaso utilize a opção de criação manual, informe o nome desejado entre aspas")
 instruc = 'Digite: \n1- Gerar um email automaticamente' 
 instruc2 = '2- Gerar um email manualmente\nResposta: '
 
@@ -30,11 +30,46 @@ def automatic():
         print('_____LOGIN: {}{}_____'.format(user,autodomain))
         print('_____SENHA: {}{}{}{}{}{}{}{}{}_____'.format(letter1,number1,letter2,number2,letter3,number3,letter4,number4,letter5,number5))
 
-
-
-def manual():
-        user = random.choice(nomes)
-        autodomain = random.choice(email)
+def manual1():
+        letter1 = random.choice(lista1)
+        number1 = random.choice(lista1)
+        symbol1 = random.choice(lista1)
+        number2 = random.choice(lista1)
+        letter3 = random.choice(lista1)
+        number3 = random.choice(lista1)
+        letter4 = random.choice(lista1)
+        symbol2 = random.choice(lista1)
+        letter5 = random.choice(lista1)
+        number5 = random.choice(lista1)
+        senha = ('SENHA: {}{}{}{}{}{}{}{}{}'.format(letter1,number1,symbol1,number2,letter3,number3,letter4,symbol2,letter5,number5))
+        print(senha)
+def manual2():
+        letter1 = random.choice(lista2)
+        number1 = random.choice(lista2)
+        symbol1 = random.choice(lista2)
+        number2 = random.choice(lista2)
+        letter3 = random.choice(lista2)
+        number3 = random.choice(lista2)
+        letter4 = random.choice(lista2)
+        symbol2 = random.choice(lista3)
+        letter5 = random.choice(lista2)
+        number5 = random.choice(lista2)
+        senha = ('SENHA: {}{}{}{}{}{}{}{}{}'.format(letter1,number1,symbol1,number2,letter3,number3,letter4,symbol2,letter5,number5))
+        print(senha)
+def manual3():
+        letter1 = random.choice(lista3)
+        number1 = random.choice(lista3)
+        symbol1 = random.choice(lista3)
+        number2 = random.choice(lista3)
+        letter3 = random.choice(lista3)
+        number3 = random.choice(lista3)
+        letter4 = random.choice(lista3)
+        symbol2 = random.choice(lista3)
+        letter5 = random.choice(lista3)
+        number5 = random.choice(lista3)
+        senha = ('SENHA: {}{}{}{}{}{}{}{}{}'.format(letter1,number1,symbol1,number2,letter3,number3,letter4,symbol2,letter5,number5))
+        print(senha)
+def manual4():
         letter1 = random.choice(lista1)
         number1 = random.choice(lista2)
         symbol1 = random.choice(lista3)
@@ -45,7 +80,8 @@ def manual():
         symbol2 = random.choice(lista3)
         letter5 = random.choice(lista1)
         number5 = random.choice(lista2)
-        senha = ('_____SENHA: {}{}{}{}{}{}{}{}{}_____'.format(letter1,number1,symbol1,number2,letter3,number3,letter4,syombol2,letter5,number5))
+        senha = ('SENHA: {}{}{}{}{}{}{}{}{}'.format(letter1,number1,symbol1,number2,letter3,number3,letter4,symbol2,letter5,number5))
+        print(senha)
 
 
 
@@ -61,31 +97,47 @@ while True:
         sleep(1)
         automatic()
         sleep(1)
-        repeat = str(input("Deseja algo mais?\Resposta: "))
+        repeat = str(input("Deseja algo mais?\nResposta: "))
         if repeat == 'n':
             print("Foi um prazer ajudar")
             sleep(0.8)
+            break;
     if instruction == 2:
         print('Vamos juntos criar seu email')
         sleep(1)
-        name = str(input("Primeiro, qual nome gostaria no seu email?\nResposta: "))
+        name = str(input("Primeiro, qual nome gostaria no seu email?\nResposta: " ))
         sleep(1)
         print("Ok, guardei o nome que você escolheu para usarmos na finalização do seu email")
         sleep(1)
-        domain = str(input("Agora vamos ao domínio desejado(gmail, hotmail, ig, etc..)\Resposta:"))
+        domain = str(input("Agora vamos ao domínio desejado(gmail, hotmail, ig, etc..)\nResposta:"))
         sleep(0.5)
         print("Ok tudo certo, domínio guardado")
-        password = str(input("Agora a parte mais importante, você gostaria que sua senha seja feita de letras, números ou símbolos especiais\n1- Letras\n2- Números\n3- Símbolos especiais\n4- Mix dos 3\nResposta:"))
+        password = "Agora a parte mais importante, você gostaria que sua senha seja feita de letras, números ou símbolos especiais\n1- Letras\n2- Números\n3- Símbolos especiais\n4- Mix dos 3\nResposta:"
         sleep(1)
+       
+
         # question2 = str(input("Bom, tudo certo até agora, sua senha terá 9 caracteres, deseja reduzir seu tamanho?\nResposta: "))
         # if question2 == 'n':
         print("Ok, gerando sua senha....")
         sleep(0.4)
-        manual()
+        # manual()
         sleep(1)
         print('')
-        print("Email: {}@{}.com")
-        print(senha)
+        emaill = ("Email: {}@{}.com".format(name, domain))
+        passwordd = int(input(password))
+        if passwordd == 1:
+                print(emaill)
+                manual1()
+        if passwordd == 2:
+                print(emaill)
+                manual2()
+        if passwordd == 3:
+                print(emaill)
+                manual3()
+        if passwordd == 4:
+                print(emaill)
+                manual4()
+        
         print('Foi um prazer ajudar na criação do seu email, tenha um bom dia!!!!')
         sleep(0.4)
         break;
